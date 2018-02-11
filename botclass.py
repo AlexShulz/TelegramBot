81fd0260029703a28c623ba134a4451aab24e9c2
Alex Prezner <alexey.preyzner@gmail.com>
2018-02-11 14:02:15 +0300

    New methods added, some old methods rewrited, code bugs fixed.

--- botclass.py
+++ botclass.py
@@ -12,7 +12,6 @@ class TelBotClass(object):
         self._token = token
         self._url = "https://api.telegram.org/bot" + self._token
 
-
     def getUpdates(self, **kwargs):
         """Get new incoming messages from bot"""
         self._offset = kwargs.get('offset', 0)
@@ -35,7 +34,6 @@ class TelBotClass(object):
         except:
             return None
 
-
     def setWebhook(self, url, **kwargs):
         data={
         'url': url
@@ -45,8 +43,6 @@ class TelBotClass(object):
             return True
         except:
             return False
-
-
 
     def deleteWebhook(self):
         """Try to delete Webhook. If success returns True, else returns False"""
@@ -65,7 +61,6 @@ class TelBotClass(object):
         except:
             return None
 
-
     def sendMessage(self, chat_id, text, **kwargs):
         """ Sending a message from bot to specified chat """
         param = {
@@ -73,10 +68,9 @@ class TelBotClass(object):
         'text': text
         }
 
-        self._response = requests.post(self._url
+        response = requests.post(self._url
                                        + '/sendMessage', data=param)
-        return self._response
-
+        return response
 
     def forwardMessage(self, chat_id,from_chat_id,
                        message_id, disable_notification=True):
@@ -87,13 +81,12 @@ class TelBotClass(object):
         'disable_notification': disable_notification
         }
         try:
-            self._response = requests.post(self._url + '/forwardMessage',
+            response = requests.post(self._url + '/forwardMessage',
                                            data=data)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendPhoto(self, chat_id, photo, **kwargs):
         data = {
@@ -107,13 +100,12 @@ class TelBotClass(object):
         'photo': open(photo, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendAudio(self, chat_id, audio, **kwargs):
         data = {
@@ -122,7 +114,7 @@ class TelBotClass(object):
         'duration': kwargs.get('duration', None),
         'performer': kwargs.get('performer', None),
         'title': kwargs.get('title', None),
-        'disable_notification': kwargs.get('disable_notification', False)
+        'disable_notification': kwargs.get('disable_notification', False),
         'reply_to_message_id': kwargs.get('reply_to_message_id', None),
         'reply_markup': kwargs.get('reply_markup', None)
         }
@@ -130,13 +122,12 @@ class TelBotClass(object):
         'audio': open(audio, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendDocument(self, chat_id, document, **kwargs):
         data = {
@@ -150,13 +141,12 @@ class TelBotClass(object):
         'document': open(document, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendVideo(self,chat_id, video, **kwargs):
         data = {
@@ -173,13 +163,12 @@ class TelBotClass(object):
         'video': open(video, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendVoice(self, chat_id, voice, **kwargs):
         data = {
@@ -194,13 +183,12 @@ class TelBotClass(object):
         'voice': open(voice, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
-
+            return response
 
     def sendVideoNote(self, chat_id, vnote, **kwargs):
         data = {
@@ -215,37 +203,49 @@ class TelBotClass(object):
         'video_note': open(vnote, 'rb')
         }
         try:
-            self._response = requests.post(self._url + '/sendPhoto',
+            response = requests.post(self._url + '/sendPhoto',
                                            data=data, files=files)
         except:
             return None
         else:
-            return self._response
+            return response
 
+    def sendMediaGroup(self, chat_id, media,
+                       reply_to_message_id=None, **kwargs):
+        data = {
+        'chat_id': chat_id,
+        'disable_notification': kwargs.get('disable_notification', False),
+        'reply_to_message_id': reply_to_message_id,
+        }
 
-    def sendMediaGroup(self, chat_id, media, **kwargs):
-        pass
-
-
-    def sendLocation(self, latitude, longtitude, **kwargs):
-        pass
-
+    def sendLocation(self, chat_id, latitude, longtitude, **kwargs):
+        data = {
+        'chat_id': chat_id,
+        'latitude': latitude,
+        'longtitude': longtitude,
+        'live_period': kwargs.get('live_period', 86400),
+        'disable_notification': kwargs.get('disable_notification', False),
+        'reply_to_message_id': kwargs.get('reply_to_message_id', None),
+        'reply_markup': kwargs.get('reply_markup', None)
+        }
+        try:
+            response = requests.post(self._url + '/sendLocation', data=data)
+        except:
+            return None
+        else:
+            return response.json()['result']
 
     def editMessageLiveLocation(self, chat_id, latitude, longtitude, **kwargs):
         pass
 
-
     def stopMessageLiveLocation(self, chat_id, **kwargs):
         pass
-
 
     def sendVenue(self, chat_id, latitude, longtitude, title, address, **kwargs):
         pass
 
-
-    def sendCOntact(self, chat_id, phone_num, first_name, **kwargs):
+    def sendContact(self, chat_id, phone_num, first_name, **kwargs):
         pass
-
 
     def sendChatAction(self, chat_id, action):
         """
@@ -260,90 +260,273 @@ class TelBotClass(object):
         'action': action
         }
         try:
-            self._response = requests.post(self._url + 'sendChatAction',
+            response = requests.post(self._url + '/sendChatAction',
                                            data=data)
         except:
             return False
         else:
-            return self._response
-
+            return True
 
     def getUserProfilePhotos(self, user_id, **kwargs):
         pass
 
-
     def kickChatMember(self, chat_id, user_id, until_date=None):
-        pass
-
+        data= {
+        'chat_id': chat_id,
+        'user_id': user_id,
+        'until_date': until_date
+        }
+        try:
+            requests.post(self._url + '/kickChatMember', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def unbanChatMember(self, chat_id, user_id):
-        pass
-
+        data = {
+        'chat_id': chat_id,
+        'user_id': user_id
+        }
+        try:
+            requests.post(self._url + '/unbanChatMember', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def restrictChatMember(self, chat_id, user_id, **kwargs):
-        pass
+        data = {
+        'chat_id': chat_id,
+        'user_id': user_id,
+        'until_date': kwargs.get('until_date', None),
+        'can_send_messages': kwargs.get('can_send_messages', False),
+        'can_send_media_messages': kwargs.get('can_send_media_messages', False),
+        'can_send_other_messages': kwargs.get('can_send_other_messages', False),
+        'can_add_web_page_previews': kwargs.get('can_add_web_page_previews',
+                                                False)
+        }
+        try:
+            requests.post(self._url + '/restrictChatMember', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def promoteChatMember(self, chat_id, user_id, **kwargs):
-        pass
-
+        data = {
+        'chat_id': chat_id,
+        'user_id': user_id,
+        'can_change_info': kwargs.get('can_change_info', True),
+        'can_post_messages': kwargs.get('can_post_messages', True),
+        'can_edit_messages': kwargs.get('can_edit_messages', True),
+        'can_delete_messages': kwargs.get('can_delete_messages', True),
+        'can_invite_users': kwargs.get('can_invite_users', True),
+        'can_restrict_members': kwargs.get('can_restrict_members', True),
+        'can_pin_messages': kwargs.get('can_pin_messages', True),
+        'can_promote_members': kwargs.get('can_promote_members', True)
+        }
+        try:
+            requests.post(self._url + '/promoteChatMember', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def exportChatInviteLink(self, chat_id):
-        pass
-
+        try:
+            response = requests.post(self._url + '/exportChatInviteLink',
+                                     data={'chat_id': chat_id})
+        except:
+            return None
+        else:
+            return response.json()['result']
 
     def setChatPhoto(self, chat_id, chat_photo):
-        pass
-
+        data = {
+        'chat_id': chat_id
+        }
+        files = {
+        'photo': open(chat_photo, 'rb')
+        }
+        try:
+            response = requests.post(self._url + '/setChatPhoto',
+                                           data=data, files=files)
+        except:
+            return False
+        else:
+            return True
 
     def deleteChatPhoto(self, chat_id):
-        pass
-
+        data = {
+        'chat_id': chat_id
+        }
+        try:
+            requests.post(self._url + '/deleteChatPhoto', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def setChatTitle(self, chat_id, title):
-        pass
+        data = {
+        'chat_id': chat_id,
+        'title': title
+        }
+        try:
+            requests.post(self._url + '/setChatTitle', data=data)
+        except:
+            return False
+        else:
+            return True
 
-
-    def setChatDescription(self, chat_id, message_id,
-                           disable_notification=True):
-        pass
-
+    def setChatDescription(self, chat_id, description):
+        data = {
+        'chat_id': chat_id,
+        'description': str(description)
+        }
+        try:
+            requests.post(self._url + '/setChatDescription', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def pinChatMessage(self, chat_id, message_id,
-                       disable_notification=True):
-        pass
-
+                       disable_notification=False):
+        data = {
+        'chat_id':chat_id,
+        'message_id': message_id,
+        'disable_notification': disable_notification
+        }
+        try:
+            requests.post(self._url + '/pinChatMessage', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def unpinChatMessage(self, chat_id):
-        pass
-
+        data = {
+        'chat_id': chat_id
+        }
+        try:
+            requests.post(self._url + '/unpinChatMessage', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def leaveChat(self, chat_id):
-        pass
-
+        data = {
+        'chat_id': chat_id
+        }
+        try:
+            requests.post(self._url + '/leaveChat', data=data)
+        except:
+            return False
+        else:
+            return True
 
     def getChat(self, chat_id):
-        pass
-
+        data = {
+        'chat_id': chat_id
+        }
+        try:
+            response = requests.post(self._url + '/getChat', data=data)
+        except:
+            return None
+        else:
+            return response.json()['result']
 
     def getChatAdministrators(self, chat_id):
-        pass
-
+        try:
+            response = requests.post(self._url + '/getChatAdministrators',
+                          data={'chat_id': chat_id})
+        except:
+            return None
+        else:
+            return response.json()['result']
 
     def getChatMembersCount(self, chat_id):
-        pass
-
+        try:
+            response = requests.post(self._url + '/getChatMembersCount',
+                                     data={'chat_id': chat_id})
+        except:
+            return None
+        else:
+            return response.json()["result"]
 
     def getChatMember(self, chat_id, member_id):
-        pass
-
+        data = {
+        'chat_id': chat_id,
+        'member_id': int(member_id)
+        }
+        try:
+            response = requests.post(self._url + 'getChatMember',data=data)
+        except:
+            return None
+        else:
+            response.json()['result']
 
     def setChatStickerSet(self, chat_id, sticker_set_name):
-        pass
-
+        data = {
+        'chat_id': chat_id,
+        'sticker_set_name': sticker_set_name
+        }
+        if self.getChat(chat_id):
+            chat = self.getChat(chat_id)
+            if ('can_set_sticker_set' in list(chat.keys()) and
+                chat['can_set_sticker_set'] == True):
+                try:
+                    requests.post(self._url + 'setChatStickerSet', data=data)
+                except:
+                    return False
+                else:
+                    return True
+            else:
+                return False
+        else:
+            return False
 
     def deleteChatStickerSet(self, chat_id):
-        pass
-
+        if self.getChat(chat_id):
+            chat = self.getChat(chat_id)
+            if ('can_set_sticker_set' in list(chat.keys()) and
+                chat['can_set_sticker_set'] == True):
+                try:
+                    requests.post(self._url + '/deleteChatStickerSet',
+                                  data={'chat_id': chat_id})
+                except:
+                    return False
+                else:
+                    return True
+            else:
+                return False
+        else:
+            return False
 
     def answerCallbackQuery(self, callback_query_id, **kwargs):
         pass
+
+    def answerInlineQuery(inline_query_id, results, **kwargs):
+        pass
+
+    def editMessageText(self, text, **kwargs):
+        pass
+
+    def editMessageCaption(self, **kwargs):
+        pass
+
+    def editMessageReplyMarkup(self, **kwargs):
+        pass
+
+    def deleteMessage(self, chat_id, message_id):
+        try:
+            requests.post(self._url + '/deleteMessage',
+                          data = {'chat_id': chat_id,'message_id': message_id})
+        except:
+            return False
+        else:
+            return True
+            
